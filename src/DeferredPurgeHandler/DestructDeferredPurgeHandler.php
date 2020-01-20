0609b25f8f404525c0d47608ff67b8023b6eff1b
Aleksandr Denisyuk <denisyukphp@gmail.com>
2020-01-20 09:56:43 +0300

    Added DestructDeferredPurgeHandler

---
+++ src/DeferredPurgeHandler/DestructDeferredPurgeHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace TmpFile\DeferredPurgeHandler;
+
+use TmpFile\TmpFileManager;
+
+class DestructDeferredPurgeHandler implements DeferredPurgeHandlerInterface
+{
+    public function __invoke(TmpFileManager $tmpFileManager): void
+    {
+        $callback = function () use ($tmpFileManager) {
+            $tmpFileManager->purge();
+        };
+
+        new class($callback)
+        {
+            private $callback;
+
+            public function __construct(callable $callback)
+            {
+                $this->callback = $callback;
+            }
+
+            public function __destruct()
+            {
+                call_user_func($this->callback);
+            }
+        };
+    }
+}
