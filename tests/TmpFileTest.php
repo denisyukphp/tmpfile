508c2d0348b28a750d16557520a817d7f5ffb752
Aleksandr Denisyuk <denisyukphp@gmail.com>
2020-05-24 13:07:38 +0300

    Upgrade tests/

--- tests/TmpFileTest.php
+++ tests/TmpFileTest.php
@@ -1,14 +1,33 @@
 <?php
 
 use TmpFile\TmpFile;
-use PHPUnit\Framework\TestCase;
 
-class TmpFileTest extends TestCase
+class TmpFileTest extends \PHPUnit\Framework\TestCase
 {
-    public function testInit(): void
-    {
-        $tmpFile = new TmpFile();
+    /** @var TmpFile */
+    private $tmpFile;
 
+    public function setUp()
+    {
+        $this->tmpFile = new TmpFile();
+    }
+
+    public function testIsString(): string
+    {
+        settype($this->tmpFile, 'string');
+
+        $this->assertIsString($this->tmpFile);
+
+        return $this->tmpFile;
+    }
+
+    /**
+     * @param string $tmpFile
+     *
+     * @depends testIsString
+     */
+    public function testFileExists(string $tmpFile): void
+    {
         $this->assertFileExists($tmpFile);
     }
 }
