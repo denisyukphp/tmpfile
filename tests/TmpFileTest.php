eaee20c604d4146e7222455d8aeff4141460117c
Aleksandr Denisyuk <rb4@inbox.ru>
2021-02-25 17:03:30 +0300

    Updated tests

--- tests/TmpFileTest.php
+++ tests/TmpFileTest.php
@@ -3,6 +3,7 @@
 namespace TmpFile\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Process\PhpProcess;
 use TmpFile\TmpFile;
 
 class TmpFileTest extends TestCase
@@ -23,5 +24,38 @@ class TmpFileTest extends TestCase
         $callback();
 
         $this->assertFileNotExists($filename);
+    }
+
+    public function testRemoveTmpFileOnFatalError(): void
+    {
+        $eof = $this->getFatalErrorUseCase();
+
+        $process = new PhpProcess($eof, __DIR__);
+
+        $process->run();
+
+        $output = $process->getOutput();
+
+        $data = explode(PHP_EOL, $output);
+
+        $this->assertRegExp('~' . sys_get_temp_dir() . '~', $data[0]);
+        $this->assertFileNotExists($data[0]);
+    }
+
+    private function getFatalErrorUseCase(): string
+    {
+        return <<<'EOF'
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use TmpFile\TmpFile;
+
+$tmpFile = new TmpFile();
+
+echo (string) $tmpFile;
+
+trigger_error('Fatal error!', E_USER_ERROR);
+EOF;
     }
 }
