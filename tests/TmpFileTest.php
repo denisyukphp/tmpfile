1f090ca48c01c745258f7e699566f716b8ae5967
Aleksandr Denisyuk <rb4@inbox.ru>
2022-09-12 19:33:50 +0300

    Update tests

--- tests/TmpFileTest.php
+++ tests/TmpFileTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace TmpFile\Tests;
 
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Process\PhpProcess;
 use TmpFile\TmpFile;
 
 class TmpFileTest extends TestCase
@@ -13,45 +12,16 @@ class TmpFileTest extends TestCase
     public function testCreateTmpFileAndReturnFilename(): void
     {
         $tmpFile = new TmpFile();
-        $filenameA = (string) $tmpFile;
-        $filenameB = $tmpFile->getFilename();
 
-        $this->assertFileExists($filenameA);
-        $this->assertFileExists($filenameB);
-        $this->assertSame($filenameA, $filenameB);
+        $this->assertFileExists($tmpFile->getFilename());
     }
 
     public function testRemoveTmpFileOnGarbageCollection(): void
     {
         $filename = (function (): string {
-            return (string) new TmpFile();
+            return (new TmpFile())->getFilename();
         })();
 
         $this->assertFileDoesNotExist($filename);
-    }
-
-    public function testRemoveTmpFileOnFatalError(): void
-    {
-        $fatalErrorUseCase = <<<'EOF'
-<?php
-
-require_once __DIR__.'/../vendor/autoload.php';
-
-use TmpFile\TmpFile;
-
-$tmpFile = new TmpFile();
-
-echo $tmpFile->getFilename();
-
-trigger_error('Fatal error!', \E_USER_ERROR);
-EOF;
-
-        $process = new PhpProcess($fatalErrorUseCase, __DIR__);
-        $process->run();
-        $processOutput = $process->getOutput();
-        $data = explode(\PHP_EOL, $processOutput);
-
-        $this->assertMatchesRegularExpression('~'.sys_get_temp_dir().'~', $data[0]);
-        $this->assertFileDoesNotExist($data[0]);
     }
 }
